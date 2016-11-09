37f787f87d9963675e5b76cf1d1ad0c670c882ab
DennisDaddy <softteez95@gmail.com>
2016-11-09 13:26:08 -0800

    add files

---
+++ includes/post_content.php
@@ -0,0 +1,28 @@
+<div class="post_area">
+	 <?php
+
+
+	 $get_posts = "select * from posts order by rand() LIMIT 0,5";
+
+	   $run_posts = mysqli_query($con,$get_posts);
+
+	   while ($row_posts = mysqli_fetch_array($run_posts)){
+		 $post_id = $row_posts['post_id'];
+		 $post_title = $row_posts['post_title'];
+		 $post_date = $row_posts['post_date'];
+		 $post_author = $row_posts['post_author'];
+		 $post_image = $row_posts['post_image'];
+		 $post_content = substr($row_posts['post_content'],0,300);
+
+
+		 echo " 
+            <h2><a id='ltitle' href='details.php?post=$post_id'>$post_title</a></h2>
+            <span> <i>By:</i><strong>$post_author</strong> </span> &nbsp; <span><b> $post_date </b></span><span style='color:brown;'><b>Comments(2)</b></span>
+            <img src='admin/news_images/$post_image' width='100' height='100'/> 
+            <div>$post_content <a id='rmlink' href='details.php?post=$post_id'>Read More</a></div><br>
+            ";
+	 }
+
+	 ?>
+
+	</div>
--- index.php
+++ index.php
@@ -11,64 +11,9 @@
 		<img src="">
 	</div>
 
-	<div class="navbar">
-		<ul id="menu">
-		  <?php
-		  include("includes/database.php");
+	<?php include("includes/navbar.php");   ?>
 
-		  $get_cats = "select * from categories";
-
-		  $run_cats = mysqli_query($con,$get_cats);
-
-		  while ($cats_row =mysqli_fetch_array($run_cats)) {
-		  	$cat_id = $cats_row['cat_id'];
-		  	$cat_title = $cats_row['cat_title'];
-
-		  	echo "<li><a href='index.php?cat=$cat_id'>$cat_title</a></li>";
-		  }
- 
-
-		  ?>
-		</ul>
-		<div id="form">
-			<form method="get" action="results.php" enctype="multipart/form-data">
-			  <input type="text" name="search_query" />
-			  <input type="submit" name="search" value="Search Now" >
-				
-			</form>
-		</div>
-
-
-	</div>
-
-	<div class="post_area">
-	 <?php
-
-
-	 $get_posts = "select * from posts order by rand() LIMIT 0,5";
-
-	   $run_posts = mysqli_query($con,$get_posts);
-
-	   while ($row_posts = mysqli_fetch_array($run_posts)){
-		 $post_id = $row_posts['post_id'];
-		 $post_title = $row_posts['post_title'];
-		 $post_date = $row_posts['post_date'];
-		 $post_author = $row_posts['post_author'];
-		 $post_image = $row_posts['post_image'];
-		 $post_content = substr($row_posts['post_content'],0,300);
-
-
-		 echo " 
-            <h2><a id='ltitle' href='details.php?post=$post_id'>$post_title</a></h2>
-            <span> <i>By:</i><strong>$post_author</strong> </span> &nbsp; <span><b> $post_date </b></span><span style='color:brown;'><b>Comments(2)</b></span>
-            <img src='admin/news_images/$post_image' width='100' height='100'/> 
-            <div>$post_content <a id='rmlink' href='details.php?post=$post_id'>Read More</a></div><br>
-            ";
-	 }
-
-	 ?>
-
-	</div>
+	<?php include("includes/post_content.php");   ?>
 
 	<div class="sidebar">
 	This is post body
