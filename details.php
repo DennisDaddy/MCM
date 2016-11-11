faccaf57417bcde787c5b426bbab9880103c96a3
DennisDaddy <softteez95@gmail.com>
2016-11-11 04:20:47 -0800

    show all details

--- details.php
+++ details.php
@@ -16,27 +16,31 @@
 	<div class="post_area">
 	 <?php
 
+	 if (isset($_GET['post'])) {
 
-	 $get_posts = "select * from posts order by rand() LIMIT 0,5";
+	 	$post_id = $_GET['post'];
+
+	 $get_posts = "select * from posts where post_id='$post_id'";
 
 	   $run_posts = mysqli_query($con,$get_posts);
 
 	   while ($row_posts = mysqli_fetch_array($run_posts)){
-		 $post_id = $row_posts['post_id'];
+		 
 		 $post_title = $row_posts['post_title'];
 		 $post_date = $row_posts['post_date'];
 		 $post_author = $row_posts['post_author'];
 		 $post_image = $row_posts['post_image'];
-		 $post_content = substr($row_posts['post_content'],0,300);
+		 $post_content = $row_posts['post_content'];
 
 
 		 echo " 
-            <h2><a id='ltitle' href='details.php?post=$post_id'>$post_title</a></h2>
+            <h2>$post_title</h2>
             <span> <i>By:</i><strong>$post_author</strong> </span> &nbsp; <span><b> $post_date </b></span><span style='color:brown;'><b>Comments(2)</b></span>
-            <img src='admin/news_images/$post_image' width='100' height='100'/> 
-            <div>$post_content <a id='rmlink' href='details.php?post=$post_id'>Read More</a></div><br>
+            <img src='admin/news_images/$post_image' width='400' height='400'/> 
+            <div>$post_content</div>
             ";
 	 }
+	}
 
 	 ?>
 
